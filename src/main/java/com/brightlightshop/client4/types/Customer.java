2415ae8dc9e1a7e1da7ea81d60bf8a927f56e7ee
Ngo Quoc Binh <ngobinh0312@gmail.com>
2022-08-22 22:02:17 +0700

    fds

--- src/main/java/com/brightlightshop/client4/types/Customer.java
+++ src/main/java/com/brightlightshop/client4/types/Customer.java
@@ -2,6 +2,7 @@ package com.brightlightshop.client4.types;
 
 public abstract class Customer extends User {
     protected double balance;
+    protected int customerCode;
 
     public Customer(String _id, String firstName, String lastName, String username, String address, String phone, String password, String accountType, double balance) {
         super(_id, firstName, lastName, username, address, phone, password, accountType);
