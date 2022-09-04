53bad3c4a750ec21ac5707d73012087f8988da8c
Ngo Quoc Binh <ngobinh0312@gmail.com>
2022-09-04 09:01:45 +0700

    fix bug backend

--- src/main/java/com/brightlightshop/client4/constants/ItemConstant.java
+++ src/main/java/com/brightlightshop/client4/constants/ItemConstant.java
@@ -3,7 +3,7 @@ package com.brightlightshop.client4.constants;
 public class ItemConstant {
     public static String[] rentalTypes = {"DVD", "Record", "Game"};
 
-    public static String[] genres = {"Action", "Animation", "Historical", "Horror", "Thriller", "Crime", "Comedy"};
+    public static String[] genres = {"Action", "Animation", "Horror", "Thriller", "Crime", "Comedy"};
 
     public static String game() {
         return "game";
