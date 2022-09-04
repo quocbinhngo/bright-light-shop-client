a3de2d6e6831ea9f3f408a5351c314ef8237e9a2
Ngo Quoc Binh <ngobinh0312@gmail.com>
2022-09-04 23:06:08 +0700

    paginate view orders page

--- src/main/java/com/brightlightshop/client4/controllers/pages/ViewOrdersPageController.java
+++ src/main/java/com/brightlightshop/client4/controllers/pages/ViewOrdersPageController.java
@@ -4,15 +4,18 @@ import com.brightlightshop.client4.constants.UrlConstant;
 import com.brightlightshop.client4.controllers.components.OrderComponentController;
 import com.brightlightshop.client4.models.UserModel;
 import com.brightlightshop.client4.types.Order;
+import com.brightlightshop.client4.utils.Component;
 import com.brightlightshop.client4.utils.FXMLPath;
 import com.brightlightshop.client4.utils.JsonParser;
 import com.cloudinary.Url;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -21,6 +24,7 @@ import org.json.JSONArray;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ViewOrdersPageController implements Initializable {
@@ -30,17 +34,34 @@ public class ViewOrdersPageController implements Initializable {
     @FXML
     private HBox navigationBar;
 
+    @FXML
+    private TextField pageTextField;
+
+    @FXML
+    private void onSearchButtonClick() throws Exception {
+        setOrdersFromJson(new JSONArray(getOrdersRequest()));
+    }
+
     private final OkHttpClient client = new OkHttpClient();
 
     private ArrayList<Order> orders;
 
-    private void setOrdersFromJson(JSONArray ordersJson) {
+    private void setOrdersFromJson(JSONArray ordersJson) throws IOException {
         orders = JsonParser.getOrders(ordersJson);
+        for (Order order: orders) {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/com/brightlightshop/client4/OrderComponent.fxml"));
+
+            VBox vBox = fxmlLoader.load();
+            OrderComponentController orderComponentController = fxmlLoader.getController();
+            orderComponentController.setData(order);
+            orderContainer.getChildren().add(vBox);
+        }
     }
 
     private String getOrdersRequest() throws Exception {
         Request request = new Request.Builder()
-                .url(UrlConstant.getOrders())
+                .url(getUrl())
                 .get()
                 .addHeader("user-id", UserModel.getCurrentUser().get_id())
                 .build();
@@ -49,25 +70,35 @@ public class ViewOrdersPageController implements Initializable {
         }
     }
 
+    private String getUrl() {
+        HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(UrlConstant.getOrders())).newBuilder();
+
+        if (pageTextField.getText() == null || pageTextField.getText().equals("")) {
+            urlBuilder.addQueryParameter("page", "1");
+        } else {
+            urlBuilder.addQueryParameter("page", pageTextField.getText());
+        }
+
+        return urlBuilder.build().toString();
+    }
+
+    private void setupTextField() {
+        Component.numericTextField(pageTextField);
+        pageTextField.setText("1");
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
             setOrdersFromJson(new JSONArray(getOrdersRequest()));
             addNavigationBar();
-            for (Order order: orders) {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("/com/brightlightshop/client4/OrderComponent.fxml"));
-
-                VBox vBox = fxmlLoader.load();
-                OrderComponentController orderComponentController = fxmlLoader.getController();
-                orderComponentController.setData(order);
-                orderContainer.getChildren().add(vBox);
-            }
+            setupTextField();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+
 
     //Add navigation bar
     public void addNavigationBar(){
